cec090eff55dccfc76d745addb3f19f9e5efdfc7
Matthew Taylor <github@matthewtaylor.dev>
2020-12-01 15:23:53 +0000

    Day 01 improvements

--- 01/01.py
+++ 01/01.py
@@ -1,18 +1,15 @@
 import argparse
-
+from functools import reduce
 
 EXPENSE_VERIFICATION_NUMBER = 2020
 
 
-# Note: This solution will fail if one of the inputs is exactly half / one third of the verification number
 def main():
     expense_report = parse_input()
-
-    # Part One
-    verify_expense_report_pair(expense_report)
-
-    # Part Two
-    verify_expense_report_triple(expense_report)
+    print(f"Part 1:")
+    verify_expense_report(expense_report, 2)
+    print(f"\nPart 2:")
+    verify_expense_report(expense_report, 3)
 
 
 def parse_input():
@@ -23,45 +20,23 @@ def parse_input():
     return expense_report
 
 
-def verify_expense_report_pair(expense_report):
-    x, y = find_pair_with_total(expense_report, expense_report, EXPENSE_VERIFICATION_NUMBER)
-    if x is None or y is None:
-        print(f"Could not find pair that adds to {EXPENSE_VERIFICATION_NUMBER}")
-        return None
-    else:
-        print(f"{x} + {y} = {EXPENSE_VERIFICATION_NUMBER}")
-        verification_code = x * y
-        print(f"{x} * {y} = {verification_code}")
-        return verification_code
+def verify_expense_report(expense_report, num_entries):
+    for t in get_unique_tuples(expense_report, num_entries):
+        if sum(t) == EXPENSE_VERIFICATION_NUMBER:
+            verification_code = reduce((lambda x, y: x * y), t)
+            print(f"{' + '.join(str(x) for x in t)} = {EXPENSE_VERIFICATION_NUMBER}")
+            print(f"{' * '.join(str(x) for x in t)} = {verification_code}")
+            return verification_code
+    print(f"Could not find a solution for {num_entries} entries.")
+    return None
 
 
-def find_pair_with_total(lhs, rhs, target):
-    for x in lhs:
-        for y in rhs:
-            if x + y == target:
-                return x, y
-    return None, None
-
-
-def verify_expense_report_triple(expense_report):
-    x, y, z = find_triple_with_total(expense_report, expense_report, expense_report, EXPENSE_VERIFICATION_NUMBER)
-    if x is None or y is None or z is None:
-        print(f"Could not find triple that adds to {EXPENSE_VERIFICATION_NUMBER}")
-        return None
-    else:
-        print(f"{x} + {y} + {z} = {EXPENSE_VERIFICATION_NUMBER}")
-        verification_code = x * y * z
-        print(f"{x} * {y} * {z} = {verification_code}")
-        return verification_code
-
-
-def find_triple_with_total(list_a, list_b, list_c, target):
-    for x in list_a:
-        for y in list_b:
-            for z in list_c:
-                if x + y + z == target:
-                    return x, y, z
-    return None, None, None
+def get_unique_tuples(l, tuple_length):
+    ls = set(l)
+    unique_tuples = [(x,) for x in ls]
+    for i in range(tuple_length - 1):
+        unique_tuples = [t + (x,) for t in unique_tuples for x in ls if x not in t]
+    return unique_tuples
 
 
 if __name__ == "__main__":
