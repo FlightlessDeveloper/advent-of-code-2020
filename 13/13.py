435c519be823aa0e6c7dac34f6d52d16e09364df
Matthew Taylor <github@matthewtaylor.dev>
2020-12-14 13:36:21 +0000

    Day 13 part 2 (kinda)

--- 13/13.py
+++ 13/13.py
@@ -7,11 +7,11 @@ def main():
     # Part 1
     arrival_time, next_bus_id = find_next_bus(start_time, bus_ids)
     wait_time = arrival_time - start_time
-    print(f"Waiting {wait_time} for bus {next_bus_id}: {wait_time * next_bus_id}")
+    print(f"Waiting {wait_time} minutes for bus #{next_bus_id} ({wait_time * next_bus_id})")
 
     # Part 2
-    # contest_timestamp = find_contest_timestamp(bus_ids)
-    # print(f"Contest winning timestamp: {contest_timestamp}")
+    contest_timestamp = find_contest_timestamp(bus_ids)
+    print(f"Contest winning timestamp: {contest_timestamp}")
 
 
 def parse_args():
@@ -30,6 +30,12 @@ def find_next_bus(start_time, bus_ids, max_time_threshold=999999999):
             if id is not None and current_time % id == 0:
                 return current_time, id
     raise Exception(f"Max time of {max_time_threshold} elapsed (find_next_bus)")
+
+
+def find_contest_timestamp(bus_ids):
+    ids_with_offsets = sorted(list(filter(lambda x: x[0] is not None, (map(lambda x: (x[1], x[0]), enumerate(bus_ids))))))
+    print(ids_with_offsets)
+    raise Exception("Use WolframAlpha for this because I couldn't figure out a libary to solve the simultaneous equations")
 
 
 if __name__ == "__main__":
